3438d4e1957535573e6ec814c71553b7d806ca95
Celia <celia.montes@satec.es>
2022-06-29 02:11:21 +0200

    [main] Solved problem logo

--- src/app/shared/paths.ts
+++ src/app/shared/paths.ts
@@ -1,7 +1,7 @@
 export const paths = {
   new_trip: 'new-trip',
   i18n: 'assets/i18n/',
-  images: '../assets/img',
+  images: 'assets/img',
   image_flags: 'assets/img/flags',
   trips: 'assets/jsons/trips.json',
 };
